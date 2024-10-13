d23ff93adb8140b628cef859ddfb6260d9860315
R-Grigala <r.grigalashvili777@gmail.com>
2024-10-13 23:15:08 +0400

    Solved HW2

--- HomeWork/hw2/AI_task1_2geo.py
+++ HomeWork/hw2/AI_task1_2geo.py
@@ -1,8 +1,8 @@
 import random
 
 def create_empty_maze(n):
-    """შექმნის ცარიელ ლაბირინთს, სადაც ყველა უჯრა გზა (' ')."""
-    return [['#' if i == 0 or j == 0 or i == n - 1 or j == n - 1 else ' ' 
+    """შექმნის ცარიელ ლაბირინთს, სადაც ყველა უჯრა დაბლოკილია ('#')."""
+    return [['#' if i == 0 or j == 0 or i == n - 1 or j == n - 1 else '#' 
              for j in range(n)] for i in range(n)]
 
 def generate_maze_with_path(n):
@@ -12,42 +12,51 @@ def generate_maze_with_path(n):
     # სტარტი და ფინიში
     maze[1][1] = 'S'
     maze[n - 2][n - 2] = 'F'
+
     def dfs(x, y):
+        """DFS ალგორითმით შექმნის გზებს ლაბირინთში."""
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # მარჯვნივ, ქვემოთ, მარცხნივ, ზემოთ
-        # random.shuffle(directions)
+        random.shuffle(directions)  # შემთხვევითი მიმართულებების არჩევა
 
         for dx, dy in directions:
-            nx, ny = x + dx * 2, y + dy * 2  # ორჯერ ვიხტებით (ერთი უჯრა კედელი იქნება)
-            print(nx, ny)
-            if 1 <= nx < n - 1 and 1 <= ny < n - 1 and maze[nx][ny] == ' ':
-                # გზა გავხსნათ და DFS გავაგრძელოთ
-                maze[x + dx][y + dy] = ' '  # კედლის გატეხვა
-                maze[nx][ny] = ' '  # ახალი უჯრა
+            nx, ny = x + dx * 2, y + dy * 2  # ახალი უჯრა
+
+            if 1 <= nx < n - 1 and 1 <= ny < n - 1 and maze[nx][ny] == '#':
+                maze[x + dx][y + dy] = ' ' # ორი უჯრას შორის ხსნის გზას
+                maze[nx][ny] = ' '  # ახალი უჯრა გახსნილი გზით
                 dfs(nx, ny)
 
-    dfs(1,1)
+    # გზების გენერირება
+    dfs(1, 1)
+
+    # დარწმუნდით, რომ ფინიშთან მინიმუმ ერთი გზა არსებობს
+    if maze[n - 2][n - 3] == '#' and maze[n - 3][n - 2] == '#':
+        if random.choice([True, False]):
+            maze[n - 2][n - 3] = ' '
+        else:
+            maze[n - 3][n - 2] = ' '
+
     return maze
 
 def print_maze(maze):
+    """ბეჭდავს ლაბირინთს ლამაზად."""
     for row in maze:
-        for cell in row:
-            print(cell, end='')  # Print each cell in the row without a newline
-        print()  # Print a newline at the end of each row
-
+        print("".join(row))
 
 def bfs(maze, start, end):
+    """მოიძებნება გზა ლაბირინთში S-დან F-მდე BFS-ით."""
     rows, cols = len(maze), len(maze[0])
-    queue = [start]  # Using a list as the queue
+    queue = [start]
     visited = set([start])
     parent_map = {}
 
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # ზემოთ, ქვემოთ, მარცხნივ, მარჯვნივ
 
     while queue:
-        x, y = queue.pop(0)  # Pop the first element to mimic queue behavior
+        x, y = queue.pop(0)
 
         if (x, y) == end:
-            # Path found, reconstruct path
+            # გზის აღდგენა
             while (x, y) != start:
                 maze[x][y] = 'O'
                 x, y = parent_map[(x, y)]
@@ -55,7 +64,7 @@ def bfs(maze, start, end):
             maze[end[0]][end[1]] = 'F'
             return True
 
-        # Explore neighbors
+        # მეზობლის შემოწმება
         for dx, dy in directions:
             nx, ny = x + dx, y + dy
 
@@ -63,14 +72,14 @@ def bfs(maze, start, end):
                 queue.append((nx, ny))
                 visited.add((nx, ny))
                 parent_map[(nx, ny)] = (x, y)
-        # print(parent_map)
-    return False  # No path found
 
+    return False  # ვერ მოიძებნა გზა
 
 def solve_maze(maze):
+    """მოიძებნება და დაბეჭდავს გზას, თუ ასეთი არსებობს."""
     start = end = None
 
-    # Find start (S) and finish (F) positions
+    # მოძებნე სტარტი (S) და ფინიში (F)
     for i in range(len(maze)):
         for j in range(len(maze[i])):
             if maze[i][j] == 'S':
@@ -79,20 +88,23 @@ def solve_maze(maze):
                 end = (i, j)
 
     if not start or not end:
-        print("Start or finish not found in the maze.")
+        print("სტარტი ან ფინიში ვერ მოიძებნა.")
         return
 
     if bfs(maze, start, end):
-        print("Path found:")
+        print("მოიძებნა გზა:")
         print_maze(maze)
     else:
-        print("No path found.")
-
+        print("გზა ვერ მოიძებნა.")
 
 # ლაბირინთის ზომა
-N = 5
+N = 15
 
 # ლაბირინთის გენერირება და დაბეჭდვა
 maze = generate_maze_with_path(N)
-print("Generated Maze:")
-solve_maze(maze)
+print("გენერირებული ლაბირინთი:")
+print_maze(maze)
+
+# ლაბირინთის ამოხსნა
+# print("\nამოხსნილი ლაბირინთი:")
+# solve_maze(maze)
